e34e3291dbb8b67745ad2b9206e6a429aa4d0c74
ShaliniR8 <1705820@kiit.ac.in>
2022-09-27 23:12:30 -0500

    initial schedule page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = { title: "CS Courses for 2018-2019" };
 const App = () => {
   const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> {schedule.title} </h1>
         <p>Hello Vite + React!</p>
         <p>
           <button onClick={() => setCount(count => count + 1)}>
