462e5cd125b8cea284950a2bc4cd622180a1bd4d
ShaliniR8 <1705820@kiit.ac.in>
2022-09-30 22:05:39 -0500

    temp change

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,40 @@ import './components/Course.css'
 import Banner from "./components/Banner.jsx"
 import CourseList from "./components/CourseList.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 
 
 
-const App = () => {
+// const App = () => {
   
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       {Banner(schedule.title)}
+//       </header>
+
+//       <div className="course-list">
+//       { Object.entries(schedule.courses).map(([id, course]) => <CourseList key={id} course={course} />) }
+//       </div>
+
+//     </div>
+//   );
+// };
+const App = () => 
+ {
+  const { data, status } = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
   return (
     <div className="App">
-      <header className="App-header">
-      {Banner(schedule.title)}
-      </header>
-
-      <div className="course-list">
-      { Object.entries(schedule.courses).map(([id, course]) => <CourseList key={id} course={course} />) }
-      </div>
-
+      {status === "error" && <p>Error fetching data</p>}
+      {status === "loading" && <p>Fetching data...</p>}
+      {status === "success" && (
+        <div>
+          {data.map((user) => (
+            <p key={user.id}>{user.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
