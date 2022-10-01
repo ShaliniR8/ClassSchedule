8b9c2180da5b9f4ae7b77eb1146a06ba088eeb13
ShaliniR8 <1705820@kiit.ac.in>
2022-09-30 22:00:43 -0500

    dummy json users for testing jsonquery

---
+++ src/utilities/fetch.js
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+
+const fetchJson = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return response.json();
+};
+
+export const useJsonQuery = (url) => {
+  const { data, isLoading, error } = useQuery(["schedule"], () => fetchJson(url));
+  return [ data, isLoading, error ];
+};
